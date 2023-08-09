f68a6d3f596eb736027c3956eac9c577284fea62
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-08-09 17:59:58 +0200

    Implements the starting of the game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './style.css';
+import { Controller } from './controller';
 
-console.log('hello world');
+const displayDiv = document.querySelector('#display');
+const button1P = document.querySelector('#button1P');
+const button2P = document.querySelector('#button2P');
+let controller;
 
+button1P.addEventListener('click', () => {
+  controller = Controller(true);
+  controller.toPlayerConfiguration();
+});
+
+button2P.addEventListener('click', () => {
+  controller = Controller(false);
+  controller.toPlayerConfiguration();
+});
